39c2962db939b9ed047b2e8fa4e93e4e6d53b51d
zaxwebs <zifidigital@gmail.com>
2024-11-03 04:32:44 +0530

    added group create form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,20 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+        <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
+            <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
+                <div class="max-w-xl">
                     {{ __("You're logged in!") }}
+                </div>
+            </div>
+
+            <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
+                <div class="max-w-xl">
+                    <livewire:groups.create-form />
                 </div>
             </div>
         </div>
---
+++ resources/views/livewire/groups/create-form.blade.php
@@ -0,0 +1,51 @@
+<?php
+
+use Livewire\Volt\Component;
+use App\Models\Group;
+
+new class extends Component {
+    public $name = '';
+
+    public $description = '';
+
+    public function submit()
+    {
+        $this->validate([
+            'name' => 'required',
+        ]);
+
+        Group::create([
+            'name' => $this->name,
+            'description' => $this->description,
+        ]);
+
+        $this->reset();
+    }
+}; ?>
+
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('Create Group') }}
+        </h2>
+    </header>
+    <form wire:submit.prevent="submit" class="mt-6 space-y-6">
+        <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input wire:model="name" id="name" name="name" type="text" class="block w-full mt-1"
+                placeholder="Enter name" required autofocus />
+            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="description" :value="__('Description')" />
+            <x-text-input wire:model="description" id="description" name="description" class="block w-full mt-1"
+                placeholder="Enter description" />
+            <x-input-error class="mt-2" :messages="$errors->get('description')" />
+        </div>
+
+        <div class="flex items-center gap-4 mt-4">
+            <x-primary-button>{{ __('Submit') }}</x-primary-button>
+        </div>
+    </form>
+</section>
