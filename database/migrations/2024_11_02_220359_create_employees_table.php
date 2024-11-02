c05240406d526a9b7bf8a27dd3cb93a574fec0d4
zaxwebs <zifidigital@gmail.com>
2024-11-03 03:37:39 +0530

    added employee and group models and migrations

---
+++ database/migrations/2024_11_02_220359_create_employees_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Group;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+			$table->string('name');
+			$table->string('phone')->unique();
+			$table->foreignIdFor(Group::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
