ae1d4d59e63130a6c0ca57b1205c2fbd09c3c7de
zaxwebs <zifidigital@gmail.com>
2024-11-03 04:43:02 +0530

    removed the you are logged in message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,11 +6,6 @@
     </x-slot>
     <div class="py-12">
         <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
-            <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
-                <div class="max-w-xl">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
 
             <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
                 <div class="max-w-xl">
