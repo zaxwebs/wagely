c2d5fe0483fc1ebf3ffc42eb4743e01e71a99b39
zaxwebs <zifidigital@gmail.com>
2024-11-10 20:03:25 +0530

    added reports.create

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,13 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::view('reports/create', 'reports.create')
+    ->middleware(['auth', 'verified'])
+    ->name('reports.create');
 
 require __DIR__.'/auth.php';
